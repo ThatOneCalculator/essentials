2c65f74ca29f7c15321781b199c13ba5da7a1fef
sameerasw <sameerasw.com@gmail.com>
2026-01-16 22:51:09 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.sameerasw.essentials"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "10.0"
+        versionCode = 22
+        versionName = "10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
